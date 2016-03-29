6f6d3f2491f62bca00e06c68edd0932e9b453aa3
Victor Perez <victor.perez.martos@est.fib.upc.edu>
2016-03-29 19:31:56 +0200

    fixed bug

--- weather.js
+++ weather.js
@@ -4,11 +4,11 @@ function showCities () {
 
    req = new XMLHttpRequest();
    req.open('GET', "http://localhost/waslab04/city_names.php" + "?country=" + country, true);
-   
    req.onreadystatechange = function() { 
      if (req.readyState == 4 && req.status == 200) {
        var v = JSON.parse(req.responseText);                
        var ciutats = v['GetCitiesByCountryResult'];
+       document.getElementById("left").innerHTML = ""; //abans d'omplenar amb les ciutats, netejem les ciutats mostrades anteriorment
        for (var i = 0 ;i < v.length; ++i) {
 	 document.getElementById("left").innerHTML = document.getElementById("left").innerHTML + "<a href='javascript:void(0);' id='" + i + "' ciudad='" + v[i].Ciudad + "' onclick=showWeather('" + i + "')>" + v[i].Ciudad + "</a><br>";
        }
