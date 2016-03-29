200a4f11c48a42746bd1723b333685f85a1e0261
Albert Suàrez <albert.suarez.molgo@est.fib.upc.edu>
2016-03-29 11:06:57 +0200

    tasca #5 showCities feta

--- weather.js
+++ weather.js
@@ -2,9 +2,23 @@
 function showCities () {
    var country = document.getElementById("countryName").value;
 
-   // Replace the two lines below with your implementation
-   var mock_text = "You should show a list of clickable cities of " + country;
-   document.getElementById("left").innerHTML = mock_text;
+
+   req = new XMLHttpRequest();
+   req.open('GET', "http://localhost/waslab04/city_names.php" + "?country=" + country, true);
+   
+   req.onreadystatechange = function() { 
+     if (req.readyState == 4 && req.status == 200) {
+       var v = JSON.parse(req.responseText);                
+       var ciutats = v['GetCitiesByCountryResult'];
+       for (var i=0 ;i < v.length; ++i) {
+	 document.getElementById("left").innerHTML=document.getElementById("left").innerHTML + "<a href='javascript:void(0);' id='" + i + "' ciudad='" + v[i].Ciudad + "' onclick=showWeather('" + i + "')>" + v[i].Ciudad + "</a><br>";
+       }
+       document.getElementById("right").innerHTML =  "&lArr;"+ " Select a city from the left menu";
+    }
+  };
+  
+  req.send();
+   
 };
 
 
